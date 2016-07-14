2991eca72f897dab6b26ea2f041c8f660275006b
mlhale <mlhale@unomaha.edu>
2016-07-14 07:56:25 -0700

    end of step 10

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import *
 #Django Rest Framework
 from rest_framework import routers
 from api import views
+from django.views.decorators.csrf import csrf_exempt
 
 #REST API routes
 router = routers.DefaultRouter()
@@ -11,6 +12,7 @@ router.register(r'likes', views.LikeViewSet)
 router.register(r'userprofiles', views.UserprofileViewSet)
 
 urlpatterns = [
+	url(r'^session/', csrf_exempt(views.Session.as_view())),
     url(r'^', include(router.urls)),
 
     #Django Rest Auth
